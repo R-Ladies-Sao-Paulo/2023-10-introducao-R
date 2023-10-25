631d116e94d5330f5d2da0f7a132a3d8aa56a02f
Beatriz Milz <milz.bea@gmail.com>
2023-10-25 18:22:50 -0300

    prepara arquivo do sorteio

--- sorteio.R
+++ sorteio.R
@@ -5,19 +5,32 @@ library(googlesheets4)
 
 # essa é a URL da tabela que usamos de lista de presença
 # e também do sorteio :)
-url_tabela <- ""
+url_tabela <- "https://docs.google.com/spreadsheets/d/1D1Xgzq9A-aat_ZAf2D3vwdAeQu4uATmYG7bOblkajdQ/edit?resourcekey#gid=2130261279"
 
 
+# aqui é necessário fazer a autenticação com o google sheets
 respostas <- read_sheet(url_tabela) |>
   clean_names()
 
 # bolsas curso-r (3 BOLSAS)
 respostas |>
-  filter(x1_bolsa_em_qualquer_curso_da_curso_r_em_2023 == "Sim") |>
-  sample_n(3)
+  filter(bolsa_no_curso_r_para_ciencia_de_dados_da_curso_r_em_2023 == "Sim") |>
+  sample_n(1)
 
 # livro: como mentir com estatística
 
+respostas |>
+  filter(livro_como_mentir_com_estatistica == "Sim") |>
+  sample_n(1)
+
 # livro: Storytelling com dados
 
+respostas |>
+  filter(livro_storytelling_com_dados == "Sim") |>
+  sample_n(1)
+
 # camiseta R-Ladies
+
+respostas |>
+  filter(camiseta_da_r_ladies_sp == "Sim") |>
+  sample_n(1)
